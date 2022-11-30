fc9e23e39db7c86571dd5655be94b044a8e91086
Malik fajar <devfajarmalik@gmail.com>
2022-11-30 19:46:43 +0700

    menambahkan paraghraf index.php

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quibusdam perspiciatis labore quam aliquam cumque rerum error dolorem ullam, enim possimus? Necessitatibus minus alias voluptas porro inventore quod ipsum! Voluptas!
+
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum cupiditate dolores officia esse dolorem tenetur alias sapiente cum repudiandae dignissimos quos, reiciendis dicta veniam dolore quam laboriosam velit laborum totam.
+    Minus laborum doloremque quasi recusandae iusto odit soluta dolorem facere aliquam hic dicta libero, ab repellat officiis illo tenetur odio quas culpa, quisquam facilis aut voluptate, obcaecati nihil? Assumenda, consectetur.
+    Excepturi quod velit nam eum, sed quo voluptate ipsa, obcaecati, esse quaerat rem sint iure delectus. Animi laborum magni minima omnis quibusdam, repudiandae dignissimos quaerat ipsa quod voluptatum, quidem dolore?
+    Magni, velit voluptates nisi rem harum modi reiciendis recusandae, ab magnam facere molestias debitis qui fugit eum laborum odit sint tempora non at quas nihil, quasi fuga beatae voluptate! Animi.
+</p>
 </body>
 </html>
